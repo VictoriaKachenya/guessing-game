d91ca7dfa49a693e2cf653f2c0d7330928e51fb0
victoriakachenia <117815293+VictoriaKachenya@users.noreply.github.com>
2023-10-18 13:01:59 +0300

    init: added guessing game file

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
     constructor() {}
-
+    this.min = min;
+    this.max = max;
     setRange(min, max) {
 
     }
 
     guess() {
-
+    this.center = Math.round((this.min + this.max) / 2);
+    return this.center
     }
 
     lower() {
-
+    this.max = this.center
     }
 
     greater() {
-
+    this.min = this.center
     }
 }
 
